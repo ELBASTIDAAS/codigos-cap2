a235d7a7f643c0e7c44e13c3ead70fb39c588418
Miguel Antonio Bastidas Verdin <75141067+ELBASTIDAAS@users.noreply.github.com>
2021-12-01 12:37:48 -0800

    Add files via upload

---
+++ tipos1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int vi , vf , i ;
+for ( i= vi ; i <= vf ; i ++ )
+{
+/*Se imprime la palabra hola*/
+cout << "Hola";
+}
+i= vi;
+while ( i <= vf )
+{
+/*Se imprime la palabra adios*/
+cout << "Adios";
+i ++;
+}
+}
